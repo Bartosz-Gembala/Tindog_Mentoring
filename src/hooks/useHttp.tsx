0043f1fc0d12ae85ef8bd5912c13628f759614d9
bartosz.gembala <bartoszgembala95@gmail.com>
2023-12-03 21:21:54 +0100

    Refactr

--- src/hooks/useHttp.tsx
+++ src/hooks/useHttp.tsx
@@ -4,22 +4,25 @@ export const useHttp = <T,>() => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const sendRequest = async (
+  const sendRequest = (
     url: string,
     requestData: RequestInit
   ): Promise<T | undefined> => {
     setIsLoading(true);
 
-    const response = await fetch(url, requestData);
-    const responseData = await response.json();
-    if (!response.ok) {
-      setError(new Error(responseData.message));
-      return;
-    }
-    if (requestData) {
-      return responseData as T;
-    }
-    setIsLoading(false);
+    return fetch(url, requestData)
+      .then((response) => {
+        if (!response.ok) {
+          setError(new Error("Something went wrong!"));
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        return responseData as T;
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return { sendRequest, error, isLoading };
